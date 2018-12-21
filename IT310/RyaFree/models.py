81b1f961a7244a2ea4f3a0f1fd25b5e485ea2077
AL <ap633@njit.edu>
2018-12-21 00:53:55 -0500

    made quick changes to names. recovered from failure that currupted the migrations folder. Luckily, never pushed it to github.

--- IT310/RyaFree/models.py
+++ IT310/RyaFree/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    questionText = models.CharField(max_length=264, unique = True)
+
+    def __str__(self):
+        return self.questionText
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    OptionName = models.CharField(max_length=264, unique=True)
+    votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.OptionName
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Option, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
