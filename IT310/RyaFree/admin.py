9886603e4419cf97f6958d9a822a119f2bc52ea8
AL <ap633@njit.edu>
2018-12-21 01:09:42 -0500

    Made necessary changes to create database entries for each model. No longer crashing.

--- IT310/RyaFree/admin.py
+++ IT310/RyaFree/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from RyaFree.models import AccessRecord,Question,Option
 # Register your models here.
+
+admin.site.register(AccessRecord)
+admin.site.register(Question)
+admin.site.register(Option)
