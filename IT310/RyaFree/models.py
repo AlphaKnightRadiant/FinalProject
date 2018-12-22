5ebe76e515410fa5805bebddfb60348cbd84e5f2
AL <ap633@njit.edu>
2018-12-21 21:45:15 -0500

    Attempted to import external library in order to get a multiple choice field model. There only exists a forms version of multiple choice field. and it doesn't interact with the database.

--- IT310/RyaFree/models.py
+++ IT310/RyaFree/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+#from multiselectfield import multiselectfield
 # Create your models here.
 class Question(models.Model):
     questionText = models.CharField(max_length=264, unique = True)
@@ -21,3 +21,20 @@ class AccessRecord(models.Model):
 
     def __str__(self):
         return str(self.date)
+
+'''
+class multiOption(models.Model):
+    CHOICES1 = (("A","Are you Free Monday?"),
+                ("B","Are You Free Teusday?"),
+                ("C","Are You Free Wednesday?"),
+                ("D","Are You Free Thursday?"),
+                ("E","Are You Free Friday?"),
+                ("F","Are You Free Saturday?"),
+                ("G","Are You Free Sunday?"),
+                                            )
+    CHOICES2 = ((1, "Yes"),
+                (2, "sorry, no!"),)
+
+    my_field = multiselectfield(choices = CHOICES1)
+    my_field2= multiselectfield(choices = CHOICES2,max_choices=3,max_length = 3)
+'''
